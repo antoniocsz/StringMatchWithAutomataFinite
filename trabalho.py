8363a57ddc00b97e06590385ae46a1c18102ea49
Antonio Santana <antcesar.santana@gmail.com>
2016-10-19 10:01:46 -0300

    funçao verificar modificada para busca_padrao; buscar_padrao retorna a lista de ocorrencias no texto

--- trabalho.py
+++ trabalho.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 
 def pega_valores():
-    quantidade = int(input)
-    texto = input()
-    padrao = input()
-    return quantidade, texto, padrao
+    # quantidade = int(input())
+    # texto = input()
+    # padrao = input()
+    # return (quantidade, texto, padrao)
+    return None
 
 def get_alfabeto(padrao):
     '''Retorna o alfabeto que está contido no padrão.'''
@@ -15,43 +16,46 @@ def get_alfabeto(padrao):
     return alfabeto
 
 
-def verifica(T, trans, m):
-    s = 0
-    for i, c in enumerate(T):
-        s = trans[s][c]
-        if s == m:
-            return i-m+1
-    return -1
+def buscar_padrao(texto, transicoes, tananho):
+    estado = 0
+    ocorrencias = []
+    for count, char in enumerate(texto):
+        estado = transicoes[estado][char]
+        if estado == tananho:
+            ocorrencias.append(count - tananho +1)
+            estado = 0
+    return ocorrencias
 
 
 #import string as st
-def transicao(padrao):
-    alphabet = get_alfabeto(padrao)
-    #alphabet = st.ascii_letters+st.punctuation+st.digits+st.whitespace
-    m = len(padrao)
-    trans = [ {c:0 for c in alphabet} for i in range(m) ]
-    for s in range(m):
-        for c in alphabet:
-            k = min(m, s+1)
-            while (padrao[:s]+c)[-k:] != padrao[:k]:
+def gerar_tabela(padrao):
+    alfabeto = get_alfabeto(padrao)
+    #alfabeto = st.ascii_letters+st.punctuation+st.digits+st.whitespace
+    TAM = len(padrao)
+    transicoes = [ {caracter : 0 for caracter in alfabeto} for i in range(TAM) ]
+    for count in range(TAM):
+        for caracter in alfabeto:
+            k = min(TAM, count+1)
+            while (padrao[:count]+caracter)[-k:] != padrao[:k]:
                 k-=1
-            trans[s][c]=k
-    return trans
+            transicoes[count][caracter]=k
+    return transicoes
+
 
 def main(valores):
     opcao = input()
     while opcao != 'e':
         if opcao == 's':
-            ocorrencias = verifca(valores[1], transicao(valores[2]), len(valores[2]))
-        if opcao == 'u'
-        
+            print (buscar_padrao('aabaababbababacaababaababababbcabcbacbabcabcbabcaabababaca', gerar_tabela('ababaca'), len('ababaca')))
+        if opcao == 'u':
+            pass
         opcao = input()
-
+    exit()
 
 # padrao = 'ababaca'
 # texto = 'aabaababbabaaabacaababaababababbcabcbacbabcabcbabcaabababaca'
 #
-# print (string_matching_FSM(texto, transicao(padrao), len(padrao)))
+# print (string_matching_FSM(texto, gerar_tabela(padrao), len(padrao)))
 
 
 if __name__ == '__main__':
